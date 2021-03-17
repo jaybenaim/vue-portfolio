77e011fb5eb4012eb4de047e053aa130a55a7028
benaimjacob@gmail.com <benaimjacob@gmail.com>
2021-03-17 17:55:18 -0400

    removed unused mixin

--- src/mixins/Theme.ts
+++ src/mixins/Theme.ts
@@ -3,13 +3,12 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      theme: localStorage.getItem('theme') || 'dark'
+      theme: this.$store.getters.getTheme
     }
   },
-  watch: {
-    '$store.state.getTheme': function watcher() {
-      console.log('sadf')
-      this.theme = this.$store.getters.getTheme
-    }
-  },
+  // watch: {
+  //   '$store.state.getTheme': function watcher() {
+  //     this.theme = this.$store.getters.getTheme
+  //   }
+  // },
 })
