6c9be3489e7c5359dd1835104ace65f345bd9277
jacobbenaim <jacobjbenaim@gmail.com>
2021-09-17 13:51:11 -0400

    updated images

--- src/views/About/components/ProjectSection/assets/data/projects.ts
+++ src/views/About/components/ProjectSection/assets/data/projects.ts
@@ -1,6 +1,8 @@
 import { IProject } from '@/lib/types/models/Project'
-import cookiesnyum from "@images/cookienyum.png"
-import highlyHandy from "@images/highly-handy.png"
+import cookiesnyum from '@images/cookienyum.png'
+import highlyHandy from '@images/highly-handy.png'
+import hackerhunter from '@images/hackerhunter.png'
+import kinsale from '@images/kinsale.png'
 
 export const $projects: IProject[] = [
   {
@@ -16,7 +18,7 @@ export const $projects: IProject[] = [
     title: 'Highly Handy Home Services',
     description: 'Home Service website with admin, booking and payment capabilities',
     image: {
-      src: 'https://jacobbenaim.ca/img/highly-handy.jpg'
+      src: highlyHandy
     },
     link: 'https://highlyhandy.com/#/'
   },
@@ -24,7 +26,7 @@ export const $projects: IProject[] = [
     title: 'Real Estate Landing Page',
     description: 'Made with Bootstrap',
     image: {
-      src: 'https://jacobbenaim.ca/img/kinsale.JPG'
+      src: kinsale
     },
     link: 'https://kinsaleroad.ca/'
   },
@@ -33,7 +35,7 @@ export const $projects: IProject[] = [
     // eslint-disable-next-line max-len
     description: 'Job Scraper where a user can compile a list of jobs from multiple job platforms, and perform real-time search\'s from a list of platforms.',
     image: {
-      src: 'https://jacobbenaim.ca/img/hacker-hunter.jpg'
+      src: hackerhunter
     },
     link: 'https://job-finder-web-scraper.herokuapp.com/'
   }
